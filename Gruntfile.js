48df3ee6035345b07ae4823664eec14c0c041f34
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2021-12-09 22:13:11 +0100

    grunt file update: browserSync, watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,42 @@
 'use strict';
 
-const { __esModule } = require("bootstrap");
+// const { __esModule } = require("bootstrap");
 
-__esModule.exports = function(grunt) {
+module.exports = function(grunt) {
 
+    require('time-grunt')(grunt);
+
+    require('jit-grunt')(grunt)
     grunt.initConfig({
-
+        sass: {
+            dist: {
+                files: {
+                    'css/style.css': 'css/styles.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
+        }
     });
+    grunt.registerTask('css',['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
 }
